9754bd1037fdbb6971aba0cfce1ce3a349c4a353
ubuntu <ubuntu@ip-172-31-32-139.ec2.internal>
2020-01-29 10:41:28 +0000

    refactoring code

--- src/main/java/com/stackroute/RepeatedCharacter.java
+++ src/main/java/com/stackroute/RepeatedCharacter.java
@@ -8,29 +8,31 @@ import java.util.TreeMap;
 
 public class RepeatedCharacter {
 
-   	//Do not print anything other than what's expected/asked in problem
-    public static void main(String[] args) {
-   	//Use Scanner to get input from console
+	//Do not print anything other than what's expected/asked in problem
+	public static void main(String[] args) {
+		//Use Scanner to get input from console
 		/*
 		 * Accept a String from the console Display the most repeated character as shown
 		 * in example section Display No characters repeated, if the String has no
 		 * repeated characters or is empty
 		 */
-    	Scanner sc = new Scanner(System.in);
-    	String str = sc.nextLine();
-    	Character character = findRepeatedCharacter(str);
-    	if(character.equals('0')) {
-    		System.out.println("No characters repeated");
-    		
-    	}else {
-    		System.out.println(character);
-    	}
-    	
-    	
+		Scanner sc = new Scanner(System.in);
+		Character character = findRepeatedCharacter(sc.nextLine());
+		if(character == null) {
+			System.out.println('0');
+		}
+		else if(character.equals('0')) {
+			System.out.println("No characters repeated");
+
+		}else {
+			System.out.println(character);
+		}
 	}
 
 	public static Character findRepeatedCharacter(String input) {
-		if(input != null  && input.trim() != "") {
+		if(input == null || input.isEmpty()) {
+			return '0';
+		}else {
 			char [] charArr = input.toCharArray();
 			Map<Character, Integer> charMap = new TreeMap<Character, Integer>();
 			for (int i = 0; i < charArr.length; i++) {
@@ -39,23 +41,21 @@ public class RepeatedCharacter {
 				}else {
 					charMap.put(charArr[i], 1);
 				}
-				
+
 			}
 			Map.Entry<Character, Integer> repeatedEntry = null;
 			for (Entry<Character, Integer> entry : charMap.entrySet()) {
 				if(repeatedEntry == null || entry.getValue().compareTo(repeatedEntry.getValue())>0) {
-					
+
 					repeatedEntry = entry;
 				}
-				
+
 			}
 			if(repeatedEntry.getValue() == 0 || repeatedEntry.getValue() == 1) {
 				return '0';
 			}else {
 				return repeatedEntry.getKey();
 			}
-			
 		}
-		return '0';
 	}
 }
